1f58b7df09e6f2f75421d0ce995aa4ead59ef724
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-07-01 17:07:26 +0800

    added Ingredients model

--- server/project/admin.py
+++ server/project/admin.py
@@ -1,3 +1,12 @@
+# Admin Interface
+
 from django.contrib import admin
+from .models import Ingredients
 
 # Register your models here.
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "quantity", "unit", "description")
+    list_filter = ("name",)
+    ordering = ("name",)
+    search_fields = ("name",)
--- server/project/models.py
+++ server/project/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from django.utils import timezone
+from djfractions.models import DecimalFractionField
+
 
 # Create your models here.
+class QuantityUnits(models.TextChoices):
+    # First value: stored value in database
+    # Second value: display name/UI label (human-readable)
+    KILOGRAM = "kg", "Kilogram"
+    GRAM = "g", "Gram"
+    LITRE = "L", "Litre"
+    PIECE = "pc", "Piece"
+    MILILITRE = "ml", "Mililitre"
+    CUPS = "cups", "Cups"
+
+class Ingredients(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, null=False)
+    quantity = DecimalFractionField(decimal_places=3, max_digits=10, null=False)
+    unit = models.CharField(max_length= 20, choices=QuantityUnits.choices, null=False)
+    description = models.CharField(max_length=255, blank=True, default="") # optional, extra info
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return f"Ingredient {self.name} ({self.id})"
