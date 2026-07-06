39731b7e5868f6e6b4292aa63cf4e1d39544c317
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-07-06 15:46:43 +0800

    Added file and folder structure for api (signup)

---
+++ client/app/api/signup/route.ts
@@ -0,0 +1,10 @@
+// API for Signup Page via Route Handler
+// From https://nextjs.org/docs/app/getting-started/route-handlers
+// "Route Handlers are defined in a route.js|ts file inside the app directory"
+// "there cannot be a route.js file at the same route segment level as page.js"
+// Folder structure auto creates route "POST /api/signup"
+
+export async function POST(request: Request) {
+
+}
+
