2fa368fcba3b0341023bfe607c03bdc021601296
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-07-07 17:32:28 +0800

    Added dummy pages for navigation in the sidebar

---
+++ client/app/(webapp)/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreatePage() {
+    return (
+        <div>
+            <main>
+                <h1>Temporary Create Page</h1>
+            </main>
+        </div>
+    )
+}
